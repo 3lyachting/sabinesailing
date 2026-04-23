88ae058eefd4e55a61e0022951bbe1e511e88a79
Sabine <admin@sabine.local>
2026-04-23 10:40:12 +0200

    Deploy rebuilt frontend assets for admin routes

--- dist/public/assets/CalendrierDisponibilites-CG-U2w14.js
+++ dist/public/assets/CalendrierDisponibilites-CG-U2w14.js
@@ -1,4 +1,4 @@
-import{c as X,r as x,j as t,C as Z,a as ee}from"./index-5ghvPTi5.js";/**
+import{c as X,r as x,j as t,C as Z,a as ee}from"./index-DGKrhjKs.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
